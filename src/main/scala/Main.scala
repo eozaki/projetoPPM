b01184032a783fef0e4de49de68c5bbea2f2097b
LP <pinto.filipe.luis@gmail.com>
2025-04-06 15:04:43 +0100

    Função T1 terminada

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,10 +1,18 @@
-import Game.{Board, Coord2D}
+import Game.{Board, Coord2D, MyRandom, randomMove}
 
 object Main {
   def main(args: Array[String]): Unit = {
     val board: Board = List(List())
     val coords = populateRows(3, 2)
-    Game(board, coords)
+    //Game(board, coords)
+
+    // teste T1
+    val coordenadas: List[Coord2D] = List((0, 0), (1, 1), (2, 2), (3, 3), (4, 4), (5, 5), (6, 6), (7, 7), (8, 8), (9, 9), (10, 10), (11, 11))
+    val seed = MyRandom(System.currentTimeMillis())
+    val (coordEsc, proxRand) = randomMove(coordenadas, seed)
+    println(s"Movimento escolhido: $coordEsc")
+
+
   }
 
   def populateRows(width: Int, height: Int): List[List[Coord2D]] = (width, height) match {
@@ -16,4 +24,5 @@ object Main {
     case (_, 0) => List()
     case (row, column) => List(List((row, column))) ++ populateColumns(row, column - 1)
   }
+
 }
