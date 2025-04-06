307ddbff0d15615f3ed006a6bb0004c0e30f93cf
erick <erickozaki@aquaporservicos.pt>
2025-04-06 11:44:06 +0100

    Classes base

--- src/main/scala/Stone.scala
+++ src/main/scala/Stone.scala
@@ -1,9 +1,28 @@
-import Stone.Stone
+import Game._
+import Game.Stone.Stone
 
-object Stone extends Enumeration {
-  type Stone = Value
-  val Black, White, Empty = Value
+case class Game(board: Board, coords: List[List[Coord2D]]) {
+  def play(board: Board, player: Stone, coord: Coord2D, lstOpenCoords: List[Coord2D]):(Option[Board], List[Coord2D]) =
+    Game.play(board, player, coord, lstOpenCoords)
 }
 
-type Board = List[List[Stone]]
-type Coord2D = (Int, Int) // (row, column)
+object Game {
+  object Stone extends Enumeration {
+    type Stone = Value
+    val Black, White, Empty = Value
+  }
+
+  type Board = List[List[Stone]]
+  type Coord2D = (Int, Int) // (row, column)
+
+  def play(board: Board, player: Stone, coord: Coord2D, lstOpenCoords: List[Coord2D]): (Option[Board], List[Coord2D]) = {
+    lstOpenCoords match {
+      case List() => throw new IllegalStateException("Posição inválida para a jogada")
+      case x :: xs if x == coord => {
+        Some()
+      }
+      case x :: xs => play(board, player, coord, xs)
+    }
+  }
+
+}
